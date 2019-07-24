89e94a489e4600c7e6d145fe3b637d64b148c46e
Wilian Misael Caamal Caamal <wiliancaamal31@gmail.com>
2019-07-24 00:13:31 -0500

    Agregando funcionalidad de cuenta regresiva

--- TomatoroApp/frmMain.Designer.cs
+++ TomatoroApp/frmMain.Designer.cs
@@ -33,9 +33,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.LbTiempo = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.Timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
@@ -73,42 +73,44 @@
             this.button3.Text = "Descanso largo";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // BtnIniciar
             // 
-            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(120, 139);
-            this.button4.Margin = new System.Windows.Forms.Padding(10);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(68, 41);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Iniciar";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button4.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Image = ((System.Drawing.Image)(resources.GetObject("BtnIniciar.Image")));
+            this.BtnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnIniciar.Location = new System.Drawing.Point(120, 139);
+            this.BtnIniciar.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(68, 41);
+            this.BtnIniciar.TabIndex = 3;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
             // 
-            // button5
+            // BtnReset
             // 
-            this.button5.Image = global::TomatoroApp.Properties.Resources.emergency_stop_button_24px;
-            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.Location = new System.Drawing.Point(208, 139);
-            this.button5.Margin = new System.Windows.Forms.Padding(10);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(70, 41);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Reset";
-            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button5.UseVisualStyleBackColor = true;
+            this.BtnReset.Image = global::TomatoroApp.Properties.Resources.emergency_stop_button_24px;
+            this.BtnReset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnReset.Location = new System.Drawing.Point(208, 139);
+            this.BtnReset.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(70, 41);
+            this.BtnReset.TabIndex = 4;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
             // 
-            // label1
+            // LbTiempo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label1.Location = new System.Drawing.Point(130, 80);
-            this.label1.Margin = new System.Windows.Forms.Padding(10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 39);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Tiempo";
+            this.LbTiempo.AutoSize = true;
+            this.LbTiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.LbTiempo.Location = new System.Drawing.Point(142, 80);
+            this.LbTiempo.Margin = new System.Windows.Forms.Padding(10);
+            this.LbTiempo.Name = "LbTiempo";
+            this.LbTiempo.Size = new System.Drawing.Size(102, 39);
+            this.LbTiempo.TabIndex = 5;
+            this.LbTiempo.Text = "00:00";
             // 
             // label2
             // 
@@ -122,15 +124,16 @@
             // Timer
             // 
             this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
             // frmMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(390, 214);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.BtnReset);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.LbTiempo);
+            this.Controls.Add(this.BtnIniciar);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -138,6 +141,7 @@
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tomatoro App";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,9 +152,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label LbTiempo;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer Timer;
     }
--- TomatoroApp/frmMain.cs
+++ TomatoroApp/frmMain.cs
@@ -12,9 +12,91 @@ namespace TomatoroApp
 {
     public partial class frmMain : Form
     {
+        private int minutos = 59;
+        private int segundos = 59;
+        string tiempoRestante = String.Empty;
+        bool pausa = false;
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            Timer.Start();
+            if (pausa == true )
+            {
+                Timer.Stop();
+                BtnIniciar.Text = "Iniciar";
+                pausa = false;
+            }
+            else
+            {
+                Timer.Start();
+                BtnIniciar.Text = "Pausar";
+                pausa = true;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (minutos.ToString().Length == 1)
+            {
+                if (segundos.ToString().Length == 1)
+                {
+                    LbTiempo.Text = $" 0{minutos}:0{segundos}";
+                }
+                else
+                {
+                    LbTiempo.Text = $" 0{minutos}:{segundos}";
+                }
+            }
+            else
+            {
+                LbTiempo.Text = $" {minutos}:{segundos}";
+            }
+
+            if (minutos != 0)
+            {
+                if (segundos != 0)
+                {
+                    segundos -= 1;
+                }
+                else
+                {
+                    minutos -= 1;
+                    segundos = 59;
+                }
+            }
+            else
+            {
+                if (segundos != 0)
+                {
+                    segundos -= 1;
+                }
+                else
+                {
+                    Timer.Stop();
+                    BtnIniciar.Text = "Iniciar";
+                    MessageBox.Show("Tiempo Terminado");
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            minutos = 1;
+            segundos = 10;
+            tiempoRestante = String.Empty;
+            pausa = false;
+            LbTiempo.Text = $" {minutos}:{segundos}";
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            segundos = 10;
+            LbTiempo.Text = $" {minutos}:{segundos}";
         }
     }
 }
